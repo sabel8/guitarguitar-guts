665a980dac4b96fd745c599ef48b7050a142c1d7
Sárándi Ábel <sarandi.abel@gmail.com>
2022-10-16 10:47:36 +0100

    BE cors

--- src/app.py
+++ src/app.py
@@ -1,27 +1,45 @@
 from flask import Flask
+import flask
+from jinja2 import Undefined
 import requests
+import flask_cors
 
 app = Flask(__name__)
-@app.route('/listGuitars/', methods=['GET', 'POST'])
+flask_cors.CORS(app)
+
+
+@app.route("/listGuitars/", methods=["GET", "POST"])
+@flask_cors.cross_origin()
 def listGuitars():
-    return requests.get('https://services.guitarguitar.co.uk/WebService/api/hackathon/guitars').json()
+    return requests.get(
+        "https://services.guitarguitar.co.uk/WebService/api/hackathon/guitars"
+    ).json()
 
-@app.route('/listCoolGuitars/', methods=['GET', 'POST'])
+
+@app.route("/listCoolGuitars/", methods=["GET", "POST"])
+@flask_cors.cross_origin()
 def listCoolGuitars():
-    return requests.get('https://services.guitarguitar.co.uk/WebService/api/hackathon/guitarswithsongs').json()
+    return requests.get(
+        "https://services.guitarguitar.co.uk/WebService/api/hackathon/guitarswithsongs"
+    ).json()
 
-@app.route('/spotify/', methods=['GET', 'POST'])
+
+@app.route("/spotify/", methods=["GET", "POST"])
+@flask_cors.cross_origin()
 def spotify():
-    return requests.post('https://accounts.spotify.com/api/token', {
-    'grant_type': 'client_credentials',
-    'client_id': '59bba2356596401cbd8bf46071610dc7',
-    'client_secret': 'a556972fe6454b999a51a9b198828d93',
-}).json()
-if __name__ == '__main__':
-    print(requests.get('https://services.guitarguitar.co.uk/WebService/api/hackathon/guitars').json())
-    app.run(host='0.0.0.0', port=105)
+    return requests.post(
+        "https://accounts.spotify.com/api/token",
+        {
+            "grant_type": "client_credentials",
+            "client_id": "59bba2356596401cbd8bf46071610dc7",
+            "client_secret": "a556972fe6454b999a51a9b198828d93",
+        },
+    ).json()
 
 
+if __name__ == "__main__":
+    # print(requests.get('https://services.guitarguitar.co.uk/WebService/api/hackathon/guitars').json())
+    app.run(host="0.0.0.0", port=105)
 
     # function gettoken(){
     #   var client_id = '59bba2356596401cbd8bf46071610dc7';
